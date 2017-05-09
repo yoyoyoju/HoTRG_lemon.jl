c9d2ce13eaf328e0ea5a10568f9516e8e2bf668c
yoju LEE <yoyoyoju@gmail.com>
2017-05-09 12:18:39 +0200

    added excuting file

---
+++ src/testQuantum.jl
@@ -0,0 +1,24 @@
+workspace()
+using HoTRG_lemon.SimulatorModule
+using HoTRG_lemon.SpinModule
+using HoTRG_lemon.LatticeModule
+using TensorOperations
+using TensorMatrices_lemon
+
+dimM = 5
+trotterparameter = 0.01
+trotteriteration = 50
+
+isinginfo = SpinInfo("quantum_ising_2",1.0e-13,0.0)
+trotterinfo = TrotterInfo(trotterparameter, trotteriteration)
+ising = QuantumIsingModel(isinginfo, trotterinfo)
+lattice = buildLattice("quantum_2d_square", ising)
+simulator = buildSimulator(lattice,dimM)
+
+fieldrange = linspace(1.0,3.5,20)
+simulatorQuantum(fieldrange, simulator)
+#tensorT = getTensorT(simulator)[1]
+#tenmatMMd = getTenmatMMd(tensorT)
+#Ul, lambdaVector, Uld = svd(tenmatMMd.matrix)
+#trunUl = truncMatrixU(Ul,dimM)
+#tensorU = matU2tenU(trunUl)
---
+++ src/test_Quantum_fractal.jl
@@ -0,0 +1,31 @@
+workspace()
+using HoTRG_lemon.SimulatorModule
+using HoTRG_lemon.SpinModule
+using HoTRG_lemon.LatticeModule
+using TensorOperations
+using TensorMatrices_lemon
+
+dimM = 3
+trotterparameter = 0.01
+trotteriteration = 50
+
+isinginfo = SpinInfo("quantum_ising_2",1.0e-13,0.0)
+trotterinfo = TrotterInfo(trotterparameter, trotteriteration)
+ising = QuantumIsingModel(isinginfo, trotterinfo)
+lattice = buildLattice("quantum_2d_fractal", ising)
+simulator = buildSimulator(lattice,dimM)
+
+# sqLattice = buildLattice("quantum_2d_square", ising)
+# sqSimulator = buildSimulator(sqLattice,dimM)
+
+# println(simulator())
+# println(sqSimulator())
+
+ fieldrange = linspace(0.1,4.5,20)
+ simulatorQuantum(fieldrange, simulator; filename = "./data/data_dim6_it5.txt")
+ # simulatorQuantum(fieldrange, sqSimulator; filename = "q2s_m6_t30_ori_m7.txt")
+#tensorT = getTensorT(simulator)[1]
+#tenmatMMd = getTenmatMMd(tensorT)
+#Ul, lambdaVector, Uld = svd(tenmatMMd.matrix)
+#trunUl = truncMatrixU(Ul,dimM)
+#tensorU = matU2tenU(trunUl)
