b7ea5cdd24860957d30127e96f3cae9fb5400942
yoju LEE <yoyoyoju@gmail.com>
2017-05-18 13:44:54 +0200

    adding comments

---
+++ src/SimulatorModule/simulator_quantum_2d_fractal_initialiteration.jl
@@ -0,0 +1,13 @@
+# to do the initialiteration
+"""
+let's think what I need
+* well, maybe I can use to get the singular values
+* maybe I can get the relative entopy by the singular values
+* do the initial iteration on the trotter direction
+* do I need to do it on the spatial direction too?
+"""
+
+"""
+	getSingularValues
+"""
+function getSingularValues
